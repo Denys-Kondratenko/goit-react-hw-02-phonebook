8ab5d30f2ccba7f19549f5a08585d60d1bfdef4c
Denys-Kondratenko <dk132652@gmail.com>
2023-05-10 22:35:47 +0300

    Почав робити крок 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Form } from './Form/Form';
+import { Section } from './Section/Section';
+import { Contacts } from './Contacts/Contacts';
+import { Layout } from './Layout/Layout';
+// import { GlobalStyle } from './GlobalStyle';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Section title="Phonebook">
+          <Form />
+        </Section>
+        <Section title="Contacts">
+          <Contacts />
+        </Section>
+        {/* <GlobalStyle /> */}
+      </Layout>
+    );
+  }
+}
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,9 @@
+export const Contacts = () => {
+  return (
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  );
+};
